6232caba4496e9967105902f78fbd9d41f9ec379
Kirill Vasilev <kirillwork56@gmail.com>
2026-08-05 12:25:38 +0300

    Add fonts

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,14 +4,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 
 public class App extends Application {
+
+    private void loadFonts() {
+        Font font = Font.loadFont(
+                Objects.requireNonNull(
+                        App.class.getResourceAsStream("/fonts/Inter_18pt-Regular.ttf")
+                ),
+                12
+        );
+
+        System.out.println(font.getFamily());
+        System.out.println(font.getName());
+
+
+        Font.loadFont(
+                Objects.requireNonNull(
+                        App.class.getResourceAsStream("/fonts/Inter_18pt-Light.ttf")
+                ),
+                12
+        );
+
+        Font.loadFont(
+                Objects.requireNonNull(
+                        App.class.getResourceAsStream("/fonts/Inter_18pt-Bold.ttf")
+                ),
+                12
+        );
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
+        loadFonts();
         FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);

0a8ec433549aef17666351d43a3309bf41e74eb3
Kirill Vasilev <kirillwork56@gmail.com>
2026-08-06 12:49:43 +0300

    Ref App file

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,7 +2,6 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -11,39 +10,24 @@ import java.util.Objects;
 
 
 public class App extends Application {
-
     private void loadFonts() {
-        Font.loadFont(
-                Objects.requireNonNull(
-                        App.class.getResourceAsStream("/fonts/Inter_18pt-Regular.ttf")
-                ),
-                12
-        );
+        Font.loadFont(Objects.requireNonNull(App.class.getResourceAsStream("/fonts/Inter_18pt-Regular.ttf")), 12);
+        Font.loadFont(Objects.requireNonNull(App.class.getResourceAsStream("/fonts/Inter_18pt-Light.ttf")), 12);
+        Font.loadFont(Objects.requireNonNull(App.class.getResourceAsStream("/fonts/Inter_18pt-Bold.ttf")), 12);
+    }
 
-        Font.loadFont(
-                Objects.requireNonNull(
-                        App.class.getResourceAsStream("/fonts/Inter_18pt-Light.ttf")
-                ),
-                12
-        );
-
-        Font.loadFont(
-                Objects.requireNonNull(
-                        App.class.getResourceAsStream("/fonts/Inter_18pt-Bold.ttf")
-                ),
-                12
-        );
+    private void loadStyles(Scene scene) {
+        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/base.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/sidebar.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/views.css")).toExternalForm());
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         loadFonts();
         FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/base.css")).toExternalForm());
-        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/sidebar.css")).toExternalForm());
-        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("/css/views.css")).toExternalForm());
+        Scene scene = new Scene(loader.load());
+        loadStyles(scene);
         stage.setScene(scene);
         stage.setTitle("Corgi Balance");
         stage.setMaximized(true);
